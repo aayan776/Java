acc4f01596831ac6e5400a4499655bef187b9e78
aayan776 <forcodingproffesion@gmail.com>
2026-02-26 16:32:15 +0600

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,30 @@
+public class Intro {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int a = 44;
+        int b = 13;
+        int sum = a + b;
+        int mul = a * b;
+        double base = 3;
+        double height = 3;
+        double area = 0.5 * base * height;
+        System.out.println("Addition: " + sum);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("Area of Triangle: " + area);
+        // int long float double String char boolean
+        int c = 123;
+        long popu = 9187123517111113L;
+        float price = 23.555f;
+        double pi = 3.141519;
+        String name = "Aayan";
+        char grade = 'A';
+        boolean isjavafun = false;
+        System.out.println("Int 4-byte:  " + c);
+        System.out.println("Long 8-byte: " + popu);
+        System.out.println("float 4-byte:  " + price);
+        System.out.println("double 8-byte: " + pi);
+        System.out.println("String: " + name);
+        System.out.println("Single character: " + grade);
+        System.out.println("boolean: " + isjavafun);
+    }
+}
